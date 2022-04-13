c8abc67e9c94fab461de5ae41b8170423105c7b5
drew <sirampr@gmail.com>
2022-04-13 17:05:38 -0400

    Remove unused files and add react-query

---
+++ apis/index.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const BASE_URL = `https://search.outdoorsy.com`;
+
+export const getRentals = async (
+  keywords: string,
+  limit: number,
+  offset: number
+) => {
+  const res = await axios.get(
+    `${BASE_URL}/rentals?filter[keywords]=${keywords}&page[limit]=${limit}&page[offset]=${offset}`,
+    {
+      headers: {},
+    }
+  );
+  return res.data;
+};
